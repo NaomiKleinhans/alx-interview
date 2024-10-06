93625d204a4216c3c56b8f6a8c8411d7d41d426a
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-10-06 12:00:33 +0200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-'''Module to return pascal triangle'''
-
-
 def pascal_triangle(n):
     '''
     Pascal's triangle
@@ -12,13 +8,13 @@ def pascal_triangle(n):
     '''
     if n <= 0:
         return []
-    
+
     triangle = []
-    
+
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
-    
+
     return triangle
