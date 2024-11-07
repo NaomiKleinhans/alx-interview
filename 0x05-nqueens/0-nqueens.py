2d4190fe25dd74e3ca417a7b430ab55ab1db2de1
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-11-07 22:30:10 +0200

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,48 +1,54 @@
 #!/usr/bin/python3
+"""Solving N Queens with Backtracing"""
 import sys
 
-def print_usage_and_exit():
-    print("Usage: nqueens N")
-    sys.exit(1)
 
-def validate_input():
-    if len(sys.argv) != 2:
-        print_usage_and_exit()
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    return N
+def nqueens(n, y, board):
+    """
+    Method: nqueens - place n queens
+            on an n by n board so that
+            no queens are attacking any
+            others.
+    Parameters: n is an int that sets
+                board size and # of queens
+    Return: All possible solutions to
+            placement, in list of lists form
+    """
+    for x in range(n):
+        hold = 0
+        for q in board:
+            if x == q[1]:
+                hold = 1
+                break
+            if y - x == q[0] - q[1]:
+                hold = 1
+                break
+            if x - q[1] == q[0] - y:
+                hold = 1
+                break
+        if hold == 0:
+            board.append([y, x])
+            if y != n - 1:
+                nqueens(n, y + 1, board)
+            else:
+                print(board)
+            del board[-1]
 
-def is_safe(board, row, col):
-    for i in range(row):
-        if board[i] == col or \
-           board[i] - i == col - row or \
-           board[i] + i == col + row:
-            return False
-    return True
-
-def solve(N, row, board, solutions):
-    if row == N:
-        solutions.append([[i, board[i]] for i in range(N)])
-        return
-    for col in range(N):
-        if is_safe(board, row, col):
-            board[row] = col
-            solve(N, row + 1, board, solutions)
-            board[row] = -1
 
 def main():
-    N = validate_input()
-    solutions = []
-    board = [-1] * N
-    solve(N, 0, board, solutions)
-    for solution in solutions:
-        print(solution)
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except Exception:
+        print('N must be a number')
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-if __name__ == "__main__":
+    nqueens(n, 0, [])
+
+if __name__ == '__main__':
     main()
