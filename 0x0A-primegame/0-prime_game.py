aa0be7c380b87db009540b1592a519257dc66414
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-12-11 05:38:56 +0200

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,66 +3,55 @@
 
 
 def isWinner(x, nums):
-    """
-    Determines the winner of the game.
+    """Function to get who has won in prime game"""
+    mariaWinsCount = 0
+    benWinsCount = 0
 
-    Args:
-        x (int): Number of rounds to be played.
-        nums (list): List of integers where each represents n for the round.
+    for num in nums:
+        roundsSet = list(range(1, num + 1))
+        primesSet = primes_in_range(1, num)
 
-    Returns:
-        str: Name of the player who won the most rounds (Maria or Ben).
-        None: If there is no winner.
-    """
-    def sieve(n):
-        """
-        Generate a list of prime numbers up to n using the Sieve of Eratosthenes.
+        if not primesSet:
+            benWinsCount += 1
+            continue
 
-        Args:
-            n (int): The limit up to which primes are generated.
+        isMariaTurns = True
 
-        Returns:
-            list: A list of prime numbers up to n.
-        """
-        primes = [True for _ in range(n+1)]
-        primes[0] = primes[1] = False  # 0 and 1 are not prime numbers
-        p = 2
-        while p * p <= n:
-            if primes[p]:
-                for i in range(p * p, n+1, p):
-                    primes[i] = False
-            p += 1
-        return [i for i in range(n+1) if primes[i]]
+        while (True):
+            if not primesSet:
+                if isMariaTurns:
+                    benWinsCount += 1
+                else:
+                    mariaWinsCount += 1
+                break
 
-    if not nums or x < 1:
-        return None
+            smallestPrime = primesSet.pop(0)
+            roundsSet.remove(smallestPrime)
 
-    maria_wins = 0
-    ben_wins = 0
+            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
 
-    max_n = max(nums)  # Get the largest n to optimize prime generation
-    primes_up_to_max = sieve(max_n)
+            isMariaTurns = not isMariaTurns
 
-    # Process each round
-    for n in nums:
-        primes_in_round = [p for p in primes_up_to_max if p <= n]
-        moves = len(primes_in_round)  # The number of moves in this round
-        if moves % 2 == 0:
-            ben_wins += 1  # Even number of moves -> Ben wins
-        else:
-            maria_wins += 1  # Odd number of moves -> Maria wins
+    if mariaWinsCount > benWinsCount:
+        return "Winner: Maria"
 
-    # Determine overall winner
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+    if mariaWinsCount < benWinsCount:
+        return "Winner: Ben"
+
+    return None
 
 
-if __name__ == "__main__":
-    # Main script for testing
-    isWinner = __import__('0-prime_game').isWinner
+def is_prime(n):
+    """Returns True if n is prime, else False."""
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+
+def primes_in_range(start, end):
+    """Returns a list of prime numbers between start and end (inclusive)."""
+    primes = [n for n in range(start, end+1) if is_prime(n)]
+    return primes
